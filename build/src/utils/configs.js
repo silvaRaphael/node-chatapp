df9185fd91fa7ded6e2f7b9643e43f7b4335d85d
silvaRaphael <raphaeltiago02@gmail.com>
2023-06-07 20:27:54 -0300

    .

--- build/src/utils/configs.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.corsOptions = void 0;
-exports.corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3000/'],
-};
